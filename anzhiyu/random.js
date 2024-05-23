bd0d0b27133fa10f8e5e80f1cc6df878da871a8f
“RockIvy” <mingyansail102@gmail.com>
2024-05-23 11:40:30 +0800

    Site updated: 2024-05-23 11:40:29

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["hello-world/","posts/always-like-this/","posts/air-conditioner/","posts/echo/","posts/hamster/","posts/i-am-er-ci-yuan-and-try-to-prove/","posts/proof-of-love/","posts/the-common-hamster/"];function toRandomPost(){
+var posts=["hello-world/","posts/air-conditioner/","posts/always-like-this/","posts/i-am-er-ci-yuan-and-try-to-prove/","posts/hamster/","posts/echo/","posts/proof-of-love/","posts/the-common-hamster/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
